9e3980352a362aa5d713712375b3e42abc8d1edb
Johnnie <jmc29@pdx.edu>
2021-05-19 14:24:46 -0700

    Setting up a way to add data to the map

--- src/components/MapPage/Map.js
+++ src/components/MapPage/Map.js
@@ -1,31 +1,38 @@
 import "../../App.css";
-import React/*, { useState }, { useEffect, useState }*/ from "react";
-//import axios from "axios";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { JSCharting } from 'jscharting-react';
 
-//const covidUrl = 'https://api.caw.sh/v3/covid-19/states';
-/*const states = 'AL,AK,AZ,AR,CA,CO,CT,DE,FL,GA,HI,ID,IL,IN,IA,KS,KY,LA,ME,MD,MA,MI,MN,MS,MO,MT,NE,NV,NH,NJ,NM,NY,NC,ND,OH,OK,OR,PA,RI,SC,SD,TN,TX,UT,VT,VA,WA,WV,WI,WY'.split(
+const covidUrl = 'https://api.caw.sh/v3/covid-19/states';
+const states = 'AL,AK,AZ,AR,CA,CO,CT,DE,FL,GA,HI,ID,IL,IN,IA,KS,KY,LA,ME,MD,MA,MI,MN,MS,MO,MT,NE,NV,NH,NJ,NM,NY,NC,ND,OH,OK,OR,PA,RI,SC,SD,TN,TX,UT,VT,VA,WA,WV,WI,WY'.split(
     ','
-);*/
-
-let config = {
-    debug: false,
-    type: 'map',
-    series: [{ id: 'usMap', map: 'us' }],
-    defaultPoint: {
-        label_text: '%stateCode'
-    }
-};
+);
 
 export const CovidMap = () => {
-    // const [data, setData] = useState([]);
+    const [data, setData] = useState([]);
 
-    // useEffect(() => {
-    //     axios.get(covidUrl).then((data) => {
-    //         console.log(data);
-    //         setData(data);
-    //     });
-    // }, []);
+    useEffect(() => {
+        axios.get(covidUrl).then((data) => {
+            console.log(data);
+            setData(data);
+        });
+    }, []);
+
+    let config = { //https://jscharting.com/tutorials/types/js-map-chart/
+        debug: false,
+        type: 'map',
+        series: [{ id: 'usMap', map: 'us' /*point: []*/}], 
+        defaultPoint: {
+            label_text: '%stateCode'
+        }
+    };
+
+    //write a function to grab data for each state and store it as a point in config
+    /*let setStateData = async () => { 
+        let seriesPts = states.map((state) => {
+            return { map: 'US.' + ...}
+        })
+    }*/
 
     return(
         <main className="container">
