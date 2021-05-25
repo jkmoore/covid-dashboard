a8da62c9f8d164cc5b1e1f48f67ef562ecb042f8
jkmoore <jkm9@pdx.edu>
2021-05-25 15:29:36 +0900

    Changed sales to cases in map, set ranges to more reasonable values

--- mapData.js
+++ mapData.js
@@ -7,17 +7,17 @@ var mapStates = 'AL,AK,AZ,AR,CA,CO,CT,DE,FL,GA,HI,ID,IL,IN,IA,KS,KY,LA,ME,MD,MA,
     palette: {
       pointValue: '{%zValue}',
       colors: ['#f7fcfd', '#e5f5f9', '#ccece6', '#99d8c9', '#66c2a4', '#41ae76', '#238b45', '#006d2c', '#00441b'],
-      ranges: { min: 0, max: 1000, interval: 200 },
-      defaultRange_legendEntry_value: '$%min - $%max'
+      ranges: { min: 0, max: 4000000, interval: 800000 },
+      defaultRange_legendEntry_value: '%min - %max'
     },
     legend: {
-      title_label_text: 'Sales',
+      title_label_text: 'Cases',
       template: '%value %icon',
       position: 'top'
     },
     defaultPoint: {
       label_text: '%stateCode',
-      tooltip: '<b>%name<b/> <br/>Sales: {%zValue:c2}'
+      tooltip: '<b>%name<b/> <br/>Cases: {%zValue}'
     },
     defaultSeries_shape_padding: 0.02,
     series: [{ id: 'usMap', map: 'us' }]
@@ -28,7 +28,7 @@ var mapStates = 'AL,AK,AZ,AR,CA,CO,CT,DE,FL,GA,HI,ID,IL,IN,IA,KS,KY,LA,ME,MD,MA,
 
   function getRandomPoints() {
     var serPoints = mapStates.map(function(arrItem) {
-      return { map: 'US.' + arrItem, z: Math.random() * 1000 };
+      return { map: 'US.' + arrItem, z: Math.random() * 4000000 };
     });
     return serPoints;
   }
